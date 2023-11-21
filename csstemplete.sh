bb5d0a8e9413ab5fb5799be1d1599515257cf67e
G-Dhanusha <118172980+G-Dhanusha@users.noreply.github.com>
2023-11-21 17:34:30 +0530

    Add files via upload

---
+++ csstemplete.sh
@@ -0,0 +1,9 @@
+sudo -i
+yum update -y
+yum install -y httpd
+systemctl start httpd
+mkdir website
+cd website
+wget https://www.free-css.com/assets/files/free-css-templates/download/page296/healet.zip 
+unzip healet.zip 
+mv healet /var/www/html/
